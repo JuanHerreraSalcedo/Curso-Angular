07acf3acf7a8d5964ea9f7382ba5b26c1a47fdb7
JHerrera5 <jherrera@existaya.com>
2022-09-08 16:09:14 -0500

    se hace peticiones y conexion a la base de datos

--- listado-personas/src/app/personas.services.ts
+++ listado-personas/src/app/personas.services.ts
@@ -1,18 +1,31 @@
 import { EventEmitter, Injectable } from "@angular/core";
+import { DataServices } from "./data.services";
 import { LogginService } from "./LogginService.service";
 import { Persona } from "./persona.model";
 
 @Injectable()
 export class PersonasService{
-    personas: Persona[] = [new Persona('Juan', 'Herrera Salcedo'), new Persona('Laura', 'Ruiz')];
+    personas: Persona[] = [];
+    // personas: Persona[] = [new Persona('Juan', 'Herrera Salcedo'), new Persona('Laura', 'Ruiz')];
 
     saludar = new EventEmitter<number>();
 
-    constructor(private logginService: LogginService){}
+    constructor(private logginService: LogginService,
+                private dataServices: DataServices){}
 
+    setPersonas(personas: Persona[]){
+      this.personas = personas;
+    }
+    obtenerPersonas(){
+      return this.dataServices.cargarPersonas();
+    }
     agregarPersona(persona:Persona){
         this.logginService.enviarMensajeAConsola("agregamos persona:" + persona.nombre)
+        if(this.personas == null){
+          this.personas = [];
+        }
         this.personas.push( persona );
+        this.dataServices.guardarPersonas(this.personas);
       }
 
       encontrarPersona(index:number){
@@ -23,6 +36,7 @@ export class PersonasService{
       let persona1 = this.personas[index];
       persona1.nombre = persona.nombre;
       persona1.apellido = persona.apellido;
+      this.dataServices.modificarPersona(index, persona);
     }
 
     eliminarPersona(index:number){
